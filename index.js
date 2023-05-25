dcadc34060bb472a8a5824059addb10739b03874
Rjk-Jami <rjkhan.1221@gmail.com>
2023-05-25 17:21:57 +0600

    uplode server

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
+const jwt = require('jsonwebtoken');
 
+
+app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 
@@ -21,14 +24,108 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) =>{
+    // console.log('hitting verify JWT')
+    // console.log(req.headers.authorization)
+ 
+    const authorization = req.headers.authorization
+    if(!authorization){
+        return res.status(401).send({error: true , message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1]
+    console.log(token , 'inside verify jwt')
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=> {
+        if(error){
+            return res.status(401).send({ error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded
+        next();
+      });
+      
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const database = client.db("carDoctor");
+        const serviceCollection = database.collection("services");
+        const bookingCollection = database.collection("bookings");
+        //JWT 
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+            console.log(token)
+            res.send({token})
 
+        })
+       //services route  
+        app.get('/services', async (req,res)=>{
+            const cursor = serviceCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            // const options = {
+        
+            //     // Include only the `title` and `imdb` fields in the returned document
+            //     // projection: { title: 1, service_id:1,price:1 },
+            //   };
+            const result = await serviceCollection.findOne(query)
+            res.send(result)
+        })
+        
+        
+        // bookings
 
+        app.get('/bookings', verifyJWT , async(req,res)=>{
+            //jwt start
+            const decoded = req.decoded
+            console.log('come back get booking', decoded)
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error : 1 , message: 'forbidden access'})
+            }
+            //jwt end
+            let query = {}
+            if(req.query?.email){
+                query = { email: req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
+        })
 
+        app.patch('/bookings/:id', async(req,res)=>{
+            const id = req.params.id
+            const updatingBooking = req.body
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                 status : updatingBooking.status
+                },
+              };
+            const result =  await bookingCollection.updateOne(filter,updateDoc)
+            res.send(result)
+            console.log(updatingBooking);
+
+        })
+
+        app.post('/bookings', async(req, res)=>{
+            const booking = req.body
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+            // console.log(booking)
+        })
+        app.delete('/bookings/:id' ,async (req, res)=>{
+            const id =req.params.id
+            const query = { _id : new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
